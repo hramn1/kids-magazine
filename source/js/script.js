a437542b40bfbadf5c87504184a1e94673afc1e6
hramn1 <stmihailov@yandex.ru>
2018-11-15 18:07:10 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-  var swiper = new Swiper('.swiper-container', {
+  var swiperFavorites = new Swiper('.swiper-container--favorites', {
     slidesPerView: 10,
     spaceBetween: 0,
     slidesPerGroup: 10,
@@ -14,3 +14,18 @@
       prevEl: '.swiper-button-prev',
     },
   });
+var swiperBrands = new Swiper('.swiper-container--brands', {
+  slidesPerView: 7,
+  spaceBetween: 10,
+  slidesPerGroup: 7,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
