bb8e613a806c3afb9c132971edd2c1b52cf1dec1
hramn1 <stmihailov@yandex.ru>
2018-11-19 17:57:06 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@
   var swiperFavorites = new Swiper('.swiper-container--favorites', {
     slidesPerView: 10,
     spaceBetween: 0,
-    slidesPerGroup: 10,
+    slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
@@ -17,7 +17,7 @@
 var swiperBrands = new Swiper('.swiper-container--brands', {
   slidesPerView: 7,
   spaceBetween: 10,
-  slidesPerGroup: 7,
+  slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
